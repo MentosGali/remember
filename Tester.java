830c56f5ba91ddc5445d62fe77790c687c155724
Soy Mentos <game.mendez447@gmail.com>
2024-08-24 21:08:11 -0600

    agregando los 3 metodos

---
+++ Tester.java
@@ -0,0 +1,10 @@
+public class Tester {
+
+    public static void main(String[] args) {
+        Op o1 = new Op();
+        o1.decirQuetal();
+        o1.despedida();
+        o1.saludo();
+    }
+
+}
