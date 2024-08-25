f332f19b9abe9c612b998e28d9d738360b8da263
Soy Mentos <game.mendez447@gmail.com>
2024-08-24 20:25:13 -0600

    agrega funcino adios

--- Op.java
+++ Op.java
@@ -3,5 +3,9 @@ public class Op {
     public void saludo() {
         System.out.println("Hola");
     }
+    // cambio para la rama secundaria
 
+    public void despedida() {
+        System.out.println("adios");
+    }
 }
